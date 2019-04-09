d397242bd1fb47add70477fa81cf5ab76ae98538
mich <mich-cook@users.noreply.github.com>
2019-04-09 11:18:51 -0700

    wire up creating and destroying mq connections. stub implementation.

---
+++ mq/messenger.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+var queueName = 'message-demo';
+var queueConnection;
+
+var producers = [];
+var consumers = [];
+
+var producerChannel;
+var consumerChannel;
+
+require('amqplib/callback_api')
+	.connect('amqp://localhost', function(error, connection) {
+		if (error !== null) { shutdown(1, `Failed to open connection to message queue.`); }
+		console.log(`Connection to message queue established.`);
+		queueConnection = connection;
+
+		connection.createChannel(function(error, channel) {
+			if (error !== null) { shutdown(2, `Failed to open channel for producers.`); }
+			console.log(`Producer channel established.`);
+			producerChannel = channel;
+		});
+
+		connection.createChannel(function(error, channel) {
+			if (error !== null) { shutdown(2, `Failed to open channel for consumers.`); }
+			console.log(`Consumer channel established.`);
+			consumerChannel = channel;
+		});
+
+	});
+
+function Producer() { };
+
+function Consumer() { };
+
+function startup() { };
+
+function shutdown(code = 0, message) {
+
+	if (message !== undefined) {
+		console.log(message);
+	}
+
+	console.log(`Shooting the messenger.`);
+
+	if (producerChannel !== undefined) {
+		console.log(`Closing the producer channel.`);
+		producerChannel.close();
+	}
+
+	if (consumerChannel !== undefined) {
+		console.log(`Closing the consumer channel.`);
+		consumerChannel.close();
+	}
+
+	if (queueConnection !== undefined) {
+		console.log(`Closing the connection to the message queue.`);
+		queueConnection.close();
+	}
+
+	process.exit(code);
+
+};
+
+process.on('SIGINT', function() {
+	console.log(`\nCaught a ctrl-c`);
+	shutdown();
+});
