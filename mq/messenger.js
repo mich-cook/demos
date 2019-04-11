e3839496c4c2aff7e21e7c8564159f4c1c0ebb3b
mich <mich-cook@users.noreply.github.com>
2019-04-10 22:24:24 -0700

    added better documentation, factored out the producer creators/destroyers, ack the messages, randomize creating and destroying producers, other cleanup

--- mq/messenger.js
+++ mq/messenger.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+//
+// top level stuff used throughout
+//
 var queueName = 'message-demo';
 var queueConnection;
 
@@ -9,6 +12,9 @@ var consumers = [];
 var producerChannel;
 var consumerChannel;
 
+//
+// Setting up the connection and channels to the message queue
+//
 require('amqplib/callback_api')
 	.connect('amqp://localhost', function(error, connection) {
 		if (error !== null) { shutdown(1, `Failed to open connection to message queue.`); }
@@ -31,6 +37,9 @@ require('amqplib/callback_api')
 
 	});
 
+//
+// Message producer and associated functions
+//
 function Producer() {
 	this.message = `Hello Demo!`;
 	this.slot = `TBD`;
@@ -48,16 +57,46 @@ function Producer() {
 	};
 };
 
+// makes a new one, starts it, then puts it in the pile with the rest
+function createProducer() {
+	var producer = new Producer();
+	producer.start();
+	producer.slot = producers.push(producer);
+	console.log(`Created a producer. We now have ${producers.length}.`);
+};
+
+// takes the last one we created and kills it
+function destroyProducer() {
+	if (producers.length === 0) {
+		console.log(`No producers to remove.`);
+		return;
+	}
+	var producer = producers.pop();
+	producer.stop();  // stop the interval message production
+	producer = undefined;  // come get me, gc
+	console.log(`Removed a producer. We now have ${producers.length}.`);
+};
+
+//
+// Message consumer and associated functions
+//
 function Consumer() {
 	this.start = function() {
 		consumerChannel.assertQueue(queueName, { durable: false });
 		consumerChannel.consume(queueName, function(message) {
-			var message = JSON.parse(message.content.toString());
-			console.log(`\tReceived message '${message.message}' from producer ${message.slot} with timestamp ${message.timestamp}.`);
-		}, { noAck: true });
+			var msg = JSON.parse(message.content.toString());
+			console.log(`\tReceived message '${msg.message}' from producer ${msg.slot} with timestamp ${msg.timestamp}.`);
+			consumerChannel.ack(message);
+		});
 	};
 };
 
+// TODO: consumer functions if we really want get to them
+
+// once we have the channels, we'll start the consumer and
+// the interval that creates/destroys producers at random
+// every three seconds. that's usually enough time to see
+// that the producers have changed by watching the slot numbers
 function startup() {
 	// wait for the second knock
 	// each channel will call startup when it's created
@@ -78,24 +117,34 @@ function startup() {
 		console.log(`Created a consumer.`);
 	}());
 
-	// first pass: create a single producer
-	(function() {
-		var producer = new Producer();
-		producer.start();
-		producer.slot = producers.push(producer);
-		console.log(`Created the producer.`);
-	}());
+	// the fun part. randomly create or destroy producers
+	// that will produce messages for our consumer. to
+	// some extent, it kinda shows how it would work
+	// if these were spread across some set of machine
+	// instances.
+	setInterval(function() {
+		if (Math.random() > 0.5) {
+			createProducer();
+		} else {
+			destroyProducer();
+		}
+	}, 3000);
 
 };
 
+// close up shop as cleanly as we can.
+// TODO: kill all the producers
+// TODO: drain the message queue and kill the consumer
 function shutdown(code = 0, message) {
 
+	// if we want to provide a reason that we're shutting down
 	if (message !== undefined) {
 		console.log(message);
 	}
 
 	console.log(`Shooting the messenger.`);
 
+	// close the channels
 	if (producerChannel !== undefined) {
 		console.log(`Closing the producer channel.`);
 		producerChannel.close();
@@ -106,16 +155,17 @@ function shutdown(code = 0, message) {
 		consumerChannel.close();
 	}
 
+	// close the connection
 	if (queueConnection !== undefined) {
 		console.log(`Closing the connection to the message queue.`);
 		queueConnection.close();
 	}
 
+	// and we're done.
 	process.exit(code);
 
 };
 
 process.on('SIGINT', function() {
-	console.log(`\nCaught a ctrl-c`);
-	shutdown();
+	shutdown(0, `\nCaught a ctrl-c`);
 });
